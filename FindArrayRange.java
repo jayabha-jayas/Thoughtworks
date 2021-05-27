4e2382a96c563279efa7135c916c4612b24bd729
jayabha-jayas <51400667+jayabha-jayas@users.noreply.github.com>
2021-05-27 16:06:16 +0530

    Add files via upload

---
+++ FindArrayRange.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FindArrayRange {
+    static int[] array;
+    public static int findRange() {
+        Arrays.sort(array);
+        return array[array.length-1]-array[0];
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arrayLength = scanner.nextInt();
+        array = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            array[i] = scanner.nextInt();
+        }
+        System.out.println("The range of the array is " + findRange());
+    }
+}
